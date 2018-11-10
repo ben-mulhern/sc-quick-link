05d3d8792dbe0c8f8cd42c86045bfc12e1af38b3
Ben Mulhern <ben@benmulhern.co.uk>
2018-11-10 10:16:27 +0000

    v1.1 - Now able to handle partial incident numbers

--- background.js
+++ background.js
@@ -1,5 +1,8 @@
 chrome.omnibox.onInputEntered.addListener(
-  function(incident) {
-    var newURL = 'https://support.jhc.co.uk/link/JHCLive/INC/' + encodeURIComponent(incident);
-    chrome.tabs.create({ url: newURL });
+  function(input) {
+    let length = input.length
+    let baseIncident = '1000000'
+    let incident = ((length >= 7) ? input : baseIncident.substring(0, 7 - length) + input)
+    let newUrl = 'https://support.jhc.co.uk/link/JHCLive/INC/' + encodeURIComponent(incident)
+    chrome.tabs.create({ url: newUrl })
   });
